d4570458f2fc503a4d35741f11e9bbbd7603c46b
Hakim Ghods <ghods.hakim@hotmail.com>
2018-05-30 23:06:31 +0430

    Updates & Cleanup

---
+++ Codehaks.Middlewares/Extentions/StaticFilesMiddlewareExtention.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class UseStaticFilesMiddlewareExtention
+    {
+        public static IApplicationBuilder UseStaticFilesFromPath(
+            this IApplicationBuilder builder, string folderPath)
+        {
+            var options = new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(folderPath)
+            };
+
+            return builder.UseMiddleware<StaticFileMiddleware>(Options.Create(options));
+        }
+    }
+}
---
+++ Codehaks.Middlewares/SendFile/SendFileMiddlewareExtention.cs
@@ -0,0 +1,27 @@
+﻿using Codehaks.Middlewares.SendFile;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class SendFileMiddlewareExtension
+    {
+        public static IApplicationBuilder UseSendFile(
+            this IApplicationBuilder builder, SendFileMiddlewareOptions options)
+        {
+            return
+            builder.UseMiddleware<SendFileMiddleware>(Options.Create(options));
+        }
+
+        public static IApplicationBuilder UseSendFile(
+            this IApplicationBuilder builder, string filePath)
+        {
+            var options = new SendFileMiddlewareOptions()
+            {
+                FilePath = filePath
+            };
+
+            return
+            builder.UseMiddleware<SendFileMiddleware>(Options.Create(options));
+        }
+    }
+}
